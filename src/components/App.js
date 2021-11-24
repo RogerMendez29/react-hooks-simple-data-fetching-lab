e8a82848b12172e7830460a794b3f9a108809815
Roger Mendez <roger61087@gmail.com>
2021-11-24 17:15:29 -0500

    commit tree 8c9ebbdcf98a7e36896d0d7fe0d0ba617273397b

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import react from "react";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setImage(data.message);
+        // console.log(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+  if (!isLoaded) return <p>Loading...</p>;
+  return (
+    <div>
+      <img src={image} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
